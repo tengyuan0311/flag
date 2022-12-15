c4b3166ceb19dd7c46091f3c72b57a85fe6f890f
TengYuan0311 <2941895419@qq.com>
2022-12-16 04:32:33 +0800

    12.16 \ 4:31 报文拼法训练左右手状态机实现，导入题库功能实现

---
+++ Assets/NPinyin-master/README.md
@@ -0,0 +1,11 @@
+汉字拼音转换 .NETCore 版本
+
+# 使用方法
+
+> Install-Package NPinyin.Core -Version 2.1.3
+
+```csharp
+var pinyin = NPinyin.Pinyin.GetPinyin("我是中国人");
+```
+
+> wo shi zhong guo ren
--- Assets/_Script/WritePose.cs
+++ Assets/_Script/WritePose.cs
@@ -1,12 +1,61 @@
+//using System;
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using NPinyin;
+using Random = UnityEngine.Random;
 
 public class WritePose : MonoBehaviour
 {
-    public TMP_Text topic;
+    public TMP_Text topicCN;
+    public TMP_Text topicPY;
+    public TextAsset txt;
+    public string[] str;
+    public Animator modelAnimator;
+    public bool leftHand=true;
+
+    private void Start()
+    {
+        str = txt.text.Split('\n');
+        int rand = Random.Range(0, 3);
+        topicCN.text = str[rand];
+        topicPY.text = NPinyin.Pinyin.GetPinyin(str[rand]);
+    }
+
+    public void RightUp()
+    {
+        if (modelAnimator.GetInteger("左手角度") == 0)
+        {
+            if (modelAnimator.GetInteger("右手角度") < 4)
+            {
+                modelAnimator.SetInteger("右手角度",modelAnimator.GetInteger("右手角度")+1);
+            }
+        }
+
+        if (modelAnimator.GetInteger("左手角度") == 1)
+        {
+            if (modelAnimator.GetInteger("右手角度") == 2)
+            {
+                modelAnimator.SetInteger("右手角度",4);
+            }
+            else if (modelAnimator.GetInteger("右手角度") < 4)
+            {
+                modelAnimator.SetInteger("右手角度",modelAnimator.GetInteger("右手角度")+1);
+            }
+        }
+    }
     
-    
-    
+
+    public void PoseCheck()
+    {
+        
+
+    }
+    private void Update()
+    {
+        PoseCheck();
+    }
 }
